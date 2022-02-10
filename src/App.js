f896a3040a6a86eaa9d6ee55ee8bebf7801167c1
tbk <kyle@techbykyle.com>
2022-02-10 13:49:17 -0700

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import PoolPump from './components/PoolPump'
+
+const App = () => {
+	return <PoolPump />
+}
+
+export default App
---
+++ src/components/PoolPump.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PoolPump = () => {
+
+    return (
+        <div className="txt_center">
+            <span className="tile-icon material-icons f125">waves</span>
+            <p>Not Running</p>
+        </div>
+    )
+}
+
+export default PoolPump
